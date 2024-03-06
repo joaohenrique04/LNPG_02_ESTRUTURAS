ff22861b4903bad5f6ef0f2edf0bfae40beca4c7
Joao Henrique Alencar <jhbfa2004@gmail.com>
2024-03-05 23:43:24 -0300

    commit atual preciso dormir quando possivel avanço mais

---
+++ Java04.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Java04 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        input.close();
+    }
+}
